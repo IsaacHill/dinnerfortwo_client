c8f484400d7d0cdf8bfe451f5f877502c9b60770
Isaac <isaachill13@gmail.com>
2018-09-06 18:37:49 +1000

    ejecting react-create-app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {graphql, QueryRenderer} from 'react-relay';
+import environment from './relay/environment';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query AppQuery {allUsers{
+            edges{
+              node{
+                name
+                id
+              }
+            }
+          }}
+        `}
+        variables={{}}
+        render={({error, props}) => {
+          if (error) {
+            return <div>Error!</div>;
+          }
+          if (!props) {
+            return <div>Loading...</div>;
+          }
+          console.log(props)
+          return <div>i retruned some good good props</div>;
+        }}
+      />
     );
   }
 }
