90893d86c93fe1dcbb658d4f2a2b6efadef12622
Isaac <isaachill13@gmail.com>
2018-09-13 19:37:03 +1000

    added very basic routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,47 @@ import React, { Component } from 'react';
 import logo from '../images/logo.svg';
 import '../styles/App.css';
 import LinkList from './LinkList'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+//  return <LinkList />
 class App extends Component {
   render() {
-    return <LinkList />
+    return (
+      <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/recipes">Recipes</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/users" component={LinkList} />
+      <Route path="/recipes" component={Recipes} />
+    </div>
+    </Router>
+    )
   }
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const Recipes = () => (
+  <div>
+    <h2>thats a spicey meatball </h2>
+  </div>
+)
+
 export default App;
